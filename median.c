c01eaab68151667ba23feca2753710439a22ca4c
TomerEiny8 <tomerkorial@gmail.com>
2025-04-28 12:01:24 +0300

    median program update

--- median.c
+++ median.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]){
 		fclose(input);
 
 	int result_index = 0;
-	for(int i=0; i<num_of_numbers/2;i++){
+	for(int i=0; i<(num_of_numbers-1)/2;i++){
 		while(numbers_histo[result_index] == 0){
 			result_index++;
 		}
