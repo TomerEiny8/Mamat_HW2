7ef64c95e04037134f507f257dd0fd7ba06faf24
TomerEiny8 <tomerkorial@gmail.com>
2025-04-28 11:37:02 +0300

    median program update

--- median.c
+++ median.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]){
 	FILE *input;
 	
 	// checking which type of input we have: file or string
-	if(argv[0] == '-'){
+	if(argv[1] == '-'){
 		input = stdin;
 	} else {
 		input = fopen(argv[1], "r");
@@ -15,10 +15,10 @@ int main(int argc, char *argv[]){
 	int numbers_histo[101];
 	int num_of_numbers = 0;
 	for(int i=0;i<101;i++){
-		numbers[i] = 0;
+		numbers_histo[i] = 0;
 	}
 	
-	int line_numbr = 0;
+	int line_number = 0;
 	char buffer[5];
 	
 	while(fgets(buffer, sizeof(buffer), input)){
@@ -26,8 +26,8 @@ int main(int argc, char *argv[]){
 		char *endptr;
 		long val = strtol(buffer, &endptr, 10);
 		
-		if((val < 0) || (val > 100)){
-			fprint(stderr, "Error at line %d: invalid input %s\n", line_number, buffer);
+		if((*endptr !='\0' && *endptr != '\n') || (val < 0) || (val > 100)){
+			fprintf(stderr, "Error at line %d: invalid input %s\n", line_number, buffer);
 			if(input != stdin)
 				fclose(input);
 			return 1;
@@ -35,12 +35,13 @@ int main(int argc, char *argv[]){
 		num_of_numbers++;
 		numbers_histo[val]++;
 	}
-	int resault_index = 0;
+	int result_index = 0;
 	for(int i=0; i<num_of_numbers/2;i++){
-		while(numbers_histo[resault_index] == 0){
-			resault_index++;
+		while(numbers_histo[result_index] == 0){
+			result_index++;
 		}
-		numbers_histo[resault_index]--;
+		numbers_histo[result_index]--;
 	}
-	return resault_index;
+	fprintf("%d", result_index);
+	return 0;
 }
